3d92c1a7e6dbb53d4eb5a99bc9b47702664696a7
SwanDPH <swan.daphne@gmail.com>
2023-01-25 10:16:21 +0100

    premier commit du Vlog

---
+++ Vlog-JS.js
@@ -0,0 +1,15 @@
+function lieu (nom, annee, genre, prix, cover){
+    this.nom = nom
+    this.annee = annee
+    this.genre = genre
+    this.prix = temps
+    this.cover = cover
+}
+
+let ilesTab=[]
+
+var grece = new lieu ("La Grèce","2021","Plage/île","€€","Destination/A/Trouver")
+var martinique = new lieu ("La Martinique","2020","Plage/île","€€€","Destination/A/Trouver")
+var portugal = new lieu ("Le Portugal","2019","Continent","€","Destination/A/Trouver")
+
+ilesTab.push(grece, martinique, portugal)
